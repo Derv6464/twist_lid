146e8174f0d2f42c48dc868d3502c6b37c6691e1
DGargan <dervla.gargan@analog.com>
2026-01-29 15:15:18 +0000

    increasing movement weight

--- source/twist_lid/twist_lid/tasks/manager_based/twist_lid/twist_lid_env_cfg.py
+++ source/twist_lid/twist_lid/tasks/manager_based/twist_lid/twist_lid_env_cfg.py
@@ -222,11 +222,11 @@ class CurriculumCfg:
     """Curriculum terms for the MDP."""
 
     action_rate = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -0.2, "num_steps": 10000}
+        func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -0.5, "num_steps": 10000}
     )
 
     joint_vel = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -0.2, "num_steps": 10000}
+        func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -0.5, "num_steps": 10000}
     )
 
     upright_penalty = CurrTerm(
